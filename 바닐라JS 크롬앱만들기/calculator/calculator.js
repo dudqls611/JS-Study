957019b773f2032f6d397f6e9e6187d9153fce6d
kim young been <51738790+dudqls611@users.noreply.github.com>
2022-09-29 03:45:15 +0900

    Add files via upload

--- 바닐라JS 크롬앱만들기/calculator/calculator.js
+++ 바닐라JS 크롬앱만들기/calculator/calculator.js
@@ -1,24 +1,58 @@
+// const calculator = {
+//   plus: function(a , b) {
+//     console.log(a + b)
+//   },
+//   minus: function(a, b) {
+//     console.log(a - b)
+//   },
+//   div: function(a , b) {
+//     console.log(a / b)
+//   },
+//   multi: function(a, b) {
+//     console.log(a * b)
+//   },
+//   power: function(a, b) {
+//     console.log(a ** b)
+//   }
+
+// }
+
+// calculator.plus(10, 2)
+// calculator.minus(10, 2)
+// calculator.div(10, 2)
+// calculator.multi(10, 2)
+// calculator.power(10, 2)
+
+// const age = 30
+// function calculateKrAge(ageOfForeigner) {
+//   ageOfForeigner + 2
+//   return "hello"
+// }
+// const KrAge = calculateKrAge(age)
+
+// console.log(KrAge)
+
 const calculator = {
-  add: function(a , b) {
-    console.log(a + b)
+  plus: function(a , b) {
+    return a + b
   },
   minus: function(a, b) {
-    console.log(a - b)
+    return a - b
   },
   div: function(a , b) {
-    console.log(a / b)
+    return a / b
   },
   multi: function(a, b) {
-    console.log(a * b)
+    return a * b
   },
   power: function(a, b) {
-    console.log(a ** b)
+    return a ** b
   }
 
 }
+const plusResult = calculator.plus(2, 3)
+const minusResult = calculator.minus(plusResult, 10)
+const multiResult = calculator.multi(10, minusResult)
+const divResult = calculator.div(multiResult, plusResult)
+const powerResult = calculator.power(divResult, minusResult)
 
-calculator.add(10, 2)
-calculator.minus(10, 2)
-calculator.div(10, 2)
-calculator.multi(10, 2)
-calculator.power(10, 2)
