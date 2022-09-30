129c5d7ac8bd44f7c7b86dd2b6d3a43d53724148
kim young been <51738790+dudqls611@users.noreply.github.com>
2022-09-30 12:46:07 +0900

    Add files via upload

--- 바닐라JS 크롬앱만들기/conditionals/app.js
+++ 바닐라JS 크롬앱만들기/conditionals/app.js
@@ -1,15 +1,30 @@
-const age = parseInt(prompt("How old are you?"))
+// const age = parseInt(prompt("How old are you?"))
 
-if (isNaN(age) || age < 0) {
-  console.log("Please write a number")
-} else if (age < 18) {
-  console.log("You are too young")
-} else if (age >= 18 && age <= 50) {
-  console.log("You can drink")
-} else if (age > 50 && age <= 80) {
-  console.log("You should exercise")
-} else if (age === 100) {
-  console.log("WOW")
-} else if (age > 80) {
-  console.log("You can do whatever you want")
+// if (isNaN(age) || age < 0) {
+//   console.log("Please write a number")
+// } else if (age < 18) {
+//   console.log("You are too young")
+// } else if (age >= 18 && age <= 50) {
+//   console.log("You can drink")
+// } else if (age > 50 && age <= 80) {
+//   console.log("You should exercise")
+// } else if (age === 100) {
+//   console.log("WOW")
+// } else if (age > 80) {
+//   console.log("You can do whatever you want")
+// }
+// const title = document.getElementById("something")
+// title.innerText = "Got you!"
+
+// // console.log(title.id)
+// console.log(title.className)
+
+// const hellos = document.getElementsByClassName("hello")
+// console.log(hellos)
+
+const title = document.querySelector("div.hello:first-child h1")
+
+function handleTitleClick() {
+  title.style.color = "blue"
 }
+title.addEventListener("click", handleTitleClick)
